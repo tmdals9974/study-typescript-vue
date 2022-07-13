6fb80fe7d14eb3c6a14830633dabe7bc76b7cb65
dev_sm <tmdals9974@naver.com>
2022-07-13 16:40:29 +0900

    [feat] vuex state type 정의

--- vue-news/src/store/index.ts
+++ vue-news/src/store/index.ts
@@ -1,22 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import getters from "./getters.js";
-import mutations from "./mutations.js";
-import actions from "./actions.js";
+// import getters from "./getters.js";
+// import mutations from "./mutations.js";
+// import actions from "./actions.js";
+import { state } from "./store";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  strict: process.env.NODE_ENV !== "production",
-  state: {
-    news: [],
-    ask: [],
-    jobs: [],
-    user: {},
-    item: {},
-    list: [],
-  },
-  getters,
-  mutations,
-  actions,
-});
+const store = {
+  state: state,
+};
+
+export default new Vuex.Store(store);
+
+// export default new Vuex.Store({
+//   strict: process.env.NODE_ENV !== "production",
+//   state: {
+//     news: [],
+//     ask: [],
+//     jobs: [],
+//     user: {},
+//     item: {},
+//     list: [],
+//   },
+//   getters,
+//   mutations,
+//   actions,
+// });
