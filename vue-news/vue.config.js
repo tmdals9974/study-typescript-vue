bb0a8597ee62aa7a004f02df3e5e172944e49569
dev_sm <tmdals9974@naver.com>
2022-07-17 18:12:03 +0900

    [refactor] 프로젝트 레벨의 vuex 타입 정의.
    vuex/types/vue.d.ts을 제거해야 정상 작동함

--- vue-news/vue.config.js
+++ vue-news/vue.config.js
@@ -1,7 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
   devServer: {
-    overlay: false,
+    // overlay: false,
   },
-});
+};
